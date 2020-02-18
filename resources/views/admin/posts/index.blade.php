5b7f45a392a65f9e4b04b963c5d21860a599317f
fedPass <federica.passanante@gmail.com>
2020-02-18 20:37:10 +0100

    visualizzo lista post lato admin

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -5,6 +5,34 @@
             <div class="col-lg-12">
                 <h1>Gestisci i tuoi post</h1>
             </div>
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">ID</th>
+                  <th scope="col">Titolo</th>
+                  <th scope="col">Slug</th>
+                  <th scope="col">Autore</th>
+                  <th scope="col">Azioni</th>
+                </tr>
+              </thead>
+              <tbody>
+                  @forelse ($posts as $post)
+                      <tr>
+                        <td>{{ $post->id }}</td>
+                        <td>{{ $post->title }}</td>
+                        <td>{{ $post->slug }}</td>
+                        <td>{{ $post->author }}</td>
+                        <td>
+                            <a class="btn btn-info" href="#">Visualizza</a>
+                        </td>
+                      </tr>
+                  @empty
+                      <tr>
+                          <td colspan="5">Non ci sono posts</td>
+                      </tr>
+                  @endforelse
+              </tbody>
+            </table>
         </div>
     </div>
 @endsection
