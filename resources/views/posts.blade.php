79fc9bf00e8063cb0b7eae7cc4c2a9c486f97f03
fedPass <federica.passanante@gmail.com>
2020-02-18 20:45:28 +0100

    visualizzazione lista post pubblica

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,6 +4,13 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1>Lista di tutti i posts</h1>
+                <ul>
+                    @forelse ($posts as $post)
+                        <li>{{ $post->title }}</li>
+                    @empty
+                        <li>Non ci sono posts da visualizzare</li>
+                    @endforelse
+                </ul>
             </div>
         </div>
     </div>
