1fb21fe090d2cc997844b33f258439398e47d957
fedPass <federica.passanante@gmail.com>
2020-02-25 17:04:15 +0100

    visualizzo tag lato public e lato admin

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -9,6 +9,17 @@
                 <p>Autore: {{ $post->author }}</p>
                 <p>Slug: {{ $post->slug }}</p>
                 <p>Categoria: {{ $post->category ? $post->category->name : 'Non ancora indicata' }}</p>
+                <p>Tag:
+                    @if (($post->tags)->isNotEmpty())
+                        @foreach ($post->tags as $tag)
+                            {{-- uso if ternario per togliere la virgola ad ultimo loop --}}
+                            {{ $tag->name }}{{ $loop->last ? '' : ',' }}
+                        @endforeach
+                    @else
+                        Non ancora inseriti
+                    @endif
+                </p>
+                <p>Categoria: {{ $post->category ? $post->category->name : 'Non ancora indicata' }}</p>
                 <p>Creato il: {{ $post->created_at }}</p>
                 <p>Modificato il: {{ $post->updated_at }}</p>
                 <a class="btn btn-info" href="{{ route('admin.posts.index') }}">Torna alla lista dei posts</a>
--- resources/views/single-post.blade.php
+++ resources/views/single-post.blade.php
@@ -4,6 +4,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1>{{ $post->title }}</h1>
+                {{-- se ho img recuperala da storage --}}
                 @if (!empty($post->cover_img))
                     <img src="{{ asset('storage/'. $post->cover_img) }}" alt="{{ $post->title }}">
                 @endif
@@ -11,6 +12,15 @@
                 {{--se ho la categoria collegati all'oggetto category e prendi la sua proprietà name --}}
                 @if (!empty($post->category))
                     <p>Categoria: <a href="{{ route('blog.category', ['slug' => $post->category->slug]) }}">{{ $post->category->name }}</a></p>
+                @endif
+                {{-- se ho dei tag collegati ciclali e stampali (!empty non funziona per le collection)--}}
+                @if (($post->tags)->isNotEmpty())
+                    <p>Tag:
+                        @foreach ($post->tags as $tag)
+                            {{-- uso if ternario per togliere la virgola ad ultimo loop --}}
+                            {{ $tag->name }}{{ $loop->last ? '' : ',' }}
+                        @endforeach
+                    </p>
                 @endif
                 <p><small> - {{ $post->author }} - </small></p>
                 <a class="btn btn-info" href="{{ route('blog') }}">Torna alla lista dei posts</a>
